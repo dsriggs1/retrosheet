1e720bcdf747c5861d7d48f6b1148f7f5c0b71cb
Sean <you@example.com>
2023-12-21 19:32:50 -0500

    Adding relative paths for wine .exe.

--- unzipped/cwbat.sh
+++ unzipped/cwbat.sh
@@ -8,8 +8,8 @@ end_year=$2
 # Loop from start year to end year
 for year in $(seq $start_year $end_year)
 do
-    wine cwevent.exe -f 0-96 -x 0-60 -y $year $year*.ev* > ../data/parsed/all$year.csv &
-    wine cwsub.exe -f 0-96 -x 0-60 -f 0-9 -y $year $year*.ev* > ../data/parsed/sub$year.csv &
-    wine cwgame.exe -f 0-83 -y $year $year*.ev* > ../data/parsed/games$year.csv &
+    wine ../programs/cwevent.exe -f 0-96 -x 0-60 -y $year $year*.ev* > ../data/parsed/all$year.csv &
+    wine ../programs/cwsub.exe -f 0-96 -x 0-60 -f 0-9 -y $year $year*.ev* > ../data/parsed/sub$year.csv &
+    wine ../programs/cwgame.exe -f 0-83 -y $year $year*.ev* > ../data/parsed/games$year.csv &
     wait
 done
